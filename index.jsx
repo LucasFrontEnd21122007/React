6c5558dd31dbfed2feac82f529bc48c678586ea5
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-09-05 14:16:15 -0300

    arquivo do projeto
    código React Simples

---
+++ index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Meu Aplicativo React</h1>
+      <p>Este é um exemplo de código JSX.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
